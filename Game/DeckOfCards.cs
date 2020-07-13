988604a43ed41d45b5f72694651dcbfc3696dd5c
budi8 <budi8@STRIX>
2020-07-13 20:16:07 +0200

    Added DeckOfCards ShuffleCards method

---
+++ Game/DeckOfCards.cs
@@ -0,0 +1,32 @@
+﻿using BattleOfCards.Player;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleOfCards.Game
+{
+    public class DeckOfCards
+    {
+        public DeckOfCards(List<Card> cards)
+        {
+            Cards = cards;
+        }
+
+        public List<Card> Cards { get; set; }
+
+        public void ShuffledCards(List<Card> cards)
+        {
+            Random rng = new Random();
+
+            int n = cards.Count;
+            while (n > 1)
+            {
+                int k = rng.Next(n + 1);
+                Card value = cards[k];
+                cards[k] = cards[n];
+                cards[n] = value;
+                n--;
+            }
+        }
+    }
+}
